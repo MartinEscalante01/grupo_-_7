8ca4507ce83c229f36061af24a06927caf66e847
Facundo Rodriguez <rodriguezfacundo07@gmail.com>
2022-05-09 00:26:27 -0300

    css margenes

--- Ecommerce/app.js
+++ Ecommerce/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const mainRouter = require('./routes/index')
 const registerRouter = require('./routes/register')
 const productCart = require('./routes/productCart.js')

7f43abfb332ec7bb6f2850120b2864fecdeead49
martin <escalantejuanmartin00@gmail.com>
2022-08-11 14:40:32 -0300

    edit

--- Ecommerce/routes/apiSportix.js
+++ Ecommerce/routes/apiSportix.js
@@ -6,6 +6,10 @@ const apiController = require('../controllers/apiController');
 
 /* GET ---------------------- */
 router.get('/', apiController.list);
+router.get('/search', apiController.search);
+router.get('/:id',apiController.show);
+router.post('/',apiController.store);
+router.delete('/:id',apiController.delete);
 
 
 

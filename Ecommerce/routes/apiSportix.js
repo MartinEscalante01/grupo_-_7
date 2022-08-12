fecb416c575529535b07e397805d4dcc801df21d
Joaquin Braconi <braconijoaquin@gmail.com>
2022-08-12 17:50:54 +0200

    api

--- Ecommerce/routes/apiSportix.js
+++ Ecommerce/routes/apiSportix.js
@@ -15,19 +15,4 @@ router.delete('/users/:id',cors(),apiController.delete);
 
 
 
-
-
-
-
-
-=======
->>>>>>> 72ba8caf51d06377fff1024ce7c2634a627db5c0
-
-router.get('/',cors(),apiController.list);
-router.get('/search',cors(),apiController.search);
-router.get('/:id',cors(),apiController.show);
-router.post('/',cors(),apiController.store);
-router.delete('/:id',cors(),apiController.delete);
-
-
 module.exports = router;

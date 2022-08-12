75a84494d873434e0ead319cbb7397c2bcc5c3ee
Joaquin Braconi <braconijoaquin@gmail.com>
2022-08-12 17:46:40 +0200

    upd

--- Ecommerce/api.js
+++ Ecommerce/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+
 
 app.use(express.urlencoded({ extended: false })); //Capturar Informacion de un Formulario en un objeto literal
 app.use(express.json()); // Convertir el objeto literal en JSON
 
 const apiSportix = require('./routes/apiSportix');
 
-app.use('/sportix', apiSportix);
+app.use('/sportix',cors(), apiSportix);
 
 // Servidor
 let port = process.env.PORT || 3030;
--- Ecommerce/routes/apiSportix.js
+++ Ecommerce/routes/apiSportix.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors')
 
 // Controller
 const apiController = require('../controllers/apiController');
 
 /* GET ---------------------- */
-router.get('/users', apiController.list);
-router.get('/users/search', apiController.search);
-router.get('/users/:id',apiController.show);
-router.post('/users',apiController.store);
-router.delete('/users/:id',apiController.delete);
+router.get('/users',cors(), apiController.list);
+router.get('/users/search',cors(), apiController.search);
+router.get('/users/:id',cors(),apiController.show);
+router.post('/users',cors(),apiController.store);
+router.delete('/users/:id',cors(),apiController.delete);
 
 
 

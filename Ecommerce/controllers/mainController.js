ce48b90834532a3edd9b9cb2cbf5889c6701e155
Joaquin Braconi <braconijoaquin@gmail.com>
2022-08-16 03:19:50 +0200

    Index

--- Ecommerce/controllers/mainController.js
+++ Ecommerce/controllers/mainController.js
@@ -1,12 +1,13 @@
 const path = '\\JSON\\products.json';
 const productos = require('../database/JSON/products.json')
+const db = require('../database/models')
+
 
 const controller = {
     home : (req,res) => {
         const product = productos.find(producto => producto.id == req.params.id)
         const recomendaciones = []
-        for (let index = 0; index < 4; index++) {
-            const index =Math.floor(Math.random() * productos.length)
+        for (let index = 0; index < productos.length; index++) {
             recomendaciones.push(productos[index])
         }
         return res.render('products/index', {  product,recomendaciones  })

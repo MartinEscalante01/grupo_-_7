2b918728d6ff864db13d7e4c28952db92c8f15bc
Facundo Rodriguez <rodriguezfacundo07@gmail.com>
2022-08-17 19:25:08 -0300

    version final

--- Ecommerce/database/config/config.js
+++ Ecommerce/database/config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "12345",
+    "password": null,
     "database": "sportix_arg",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": "12345",
+    "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
